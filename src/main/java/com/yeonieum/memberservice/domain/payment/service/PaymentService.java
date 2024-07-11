f6577c0c863483e2406cb9add45f871dfe363d83
Koo-Tae-Ho <gth0908@naver.com>
2024-07-11 21:42:57 +0900

    KAN-264 feat: 회원결제카드 삭제

--- src/main/java/com/yeonieum/memberservice/domain/payment/service/PaymentService.java
+++ src/main/java/com/yeonieum/memberservice/domain/payment/service/PaymentService.java
@@ -1,6 +1,5 @@
 package com.yeonieum.memberservice.domain.payment.service;
 
-import com.yeonieum.memberservice.domain.member.dto.MemberResponse;
 import com.yeonieum.memberservice.domain.member.entity.Member;
 import com.yeonieum.memberservice.domain.member.repository.MemberRepository;
 import com.yeonieum.memberservice.domain.payment.dto.PaymentRequest;
@@ -8,6 +7,7 @@ import com.yeonieum.memberservice.domain.payment.dto.PaymentResponse;
 import com.yeonieum.memberservice.domain.payment.entity.MemberPaymentCard;
 import com.yeonieum.memberservice.domain.payment.repository.MemberPaymentCardRepository;
 import com.yeonieum.memberservice.global.enums.ActiveStatus;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +29,7 @@ public class PaymentService {
      * @throws IllegalArgumentException 존재하지 않는 회원 ID인 경우
      * @return 회원의 결제카드 목록
      */
+    @Transactional
     public List<PaymentResponse.RetrieveMemberPaymentCardDto> retrieveMemberPaymentCards(String memberId){
 
         if(!memberRepository.existsById(memberId)){
@@ -53,6 +54,7 @@ public class PaymentService {
      * @throws IllegalStateException 등록했는 결제카드가 5개를 초과할 경우
      * @return 결제카드 등록 성공 여부
      */
+    @Transactional
     public boolean registerMemberPaymentCard(PaymentRequest.RegisterMemberPaymentCardDto registerMemberPaymentCardDto){
 
         Member targetMember = memberRepository.findById(registerMemberPaymentCardDto.getMemberId())
@@ -77,7 +79,19 @@ public class PaymentService {
         return true;
     }
 
-
-
-
+    /**
+     * 회원의 결제카드 삭제
+     * @param memberPaymentCardId 결제카드 ID
+     * @throws IllegalArgumentException 존재하지 않는 결제카드 ID인 경우
+     * @return 결제카드 삭제 성공 여부
+     */
+    @Transactional
+    public boolean deleteMemberPaymentCard(Long memberPaymentCardId) {
+        if(memberPaymentCardRepository.existsById(memberPaymentCardId)) {
+            memberPaymentCardRepository.deleteById(memberPaymentCardId);
+            return true;
+        } else {
+            throw new IllegalArgumentException("존재하지 않는 결제카드 ID 입니다.");
+        }
+    }
 }
