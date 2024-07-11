f9e07abd85a7d497c57ff7efcbfc566e4f36bc69
kimujae <dnwo0103@naver.com>
2024-07-11 21:23:32 +0900

    KAN-242 feat: 인증서비스 일반로그인 구현-JwtLoginFilter정의 및 성공핸들러 및 실패 핸들러 정의

---
+++ src/main/java/com/yeonieum/memberservice/auth/handler/JwtAuthenticationSuccessHandler.java
@@ -0,0 +1,40 @@
+package com.yeonieum.memberservice.auth.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yeonieum.memberservice.auth.util.JwtUtils;
+import com.yeonieum.memberservice.auth.userdetails.CustomUserDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    // Jwt 로그인(인증) 성공 시 호출 될 성공핸들러
+    private final JwtUtils jwtUtils;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        String userName = customUserDetails.getUsername();
+
+        String token = jwtUtils.createToken(userName);
+        String json = new ObjectMapper().writeValueAsString(token);
+        response.setContentType("application/json");
+        response = jwtUtils.addJwtToHttpOnlyCookieForSSR(token, response, null);
+        response.setStatus(HttpStatus.OK.value());
+        response.getWriter().write(json);
+        response.flushBuffer();
+    }
+}
