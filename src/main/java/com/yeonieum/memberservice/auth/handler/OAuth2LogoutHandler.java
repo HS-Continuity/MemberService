6497898aa6cc8eeb8aac6f968ac2c06eadbc52bc
kimujae <dnwo0103@naver.com>
2024-07-22 14:27:12 +0900

    KAN-471 feat: 민감정보 property 관리

--- src/main/java/com/yeonieum/memberservice/auth/handler/OAuth2LogoutHandler.java
+++ src/main/java/com/yeonieum/memberservice/auth/handler/OAuth2LogoutHandler.java
@@ -3,10 +3,10 @@ package com.yeonieum.memberservice.auth.handler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
-import static com.yeonieum.memberservice.auth.util.JwtUtils.AUTHORIZATION_HEADER;
 
 
 @RequiredArgsConstructor
@@ -14,7 +14,7 @@ public class OAuth2LogoutHandler implements LogoutHandler {
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        response.setHeader("Set-Cookie",AUTHORIZATION_HEADER +"= ; " +
+        response.setHeader("Set-Cookie", HttpHeaders.AUTHORIZATION +"= ; " +
                 "Path=/;" +
                 "Domain=localhost; " +
                 "HttpOnly; " +
--- src/main/java/com/yeonieum/memberservice/auth/util/JwtUtils.java
+++ src/main/java/com/yeonieum/memberservice/auth/util/JwtUtils.java
@@ -1,10 +1,12 @@
 package com.yeonieum.memberservice.auth.util;
 
+import com.yeonieum.memberservice.auth.userdetails.CustomUserDto;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Base64;
@@ -18,44 +20,44 @@ public class JwtUtils {
     // 토큰에서 사용자 이름 추출
     // 토큰에서 권한 정보 추출
     // 토큰 유효성 검증
-    public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer";
 
     @Value("${yeonieum.cors.domain}")
-    private static String CORS_DOMAIN;
-    private final long TOKEN_VALIDATION_TIME = 25 * 60 * 1000L; // 25분
+    private String CORS_DOMAIN = "localhost:8081";
+    @Value("${jwt.token-validation-time}")
+    private int TOKEN_VALIDATION_TIME; // 25분
 
-
-    private String secretKey = "Ddfjladlewmndacafjlkcadfklfnqlekwnfklqjcxlzjalfd"; //4 배수 자리
+    @Value("${jwt.secret-key}")
+    private String secretKey;
     private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
+
+
     @PostConstruct
     public void init() {
-        // base64형식으로 디코딩
         byte[] decodedBytes = Base64.getDecoder().decode(secretKey);
-        // 디코딩된 비밀키를 hmacSha 방식으로 암호화
         key = Keys.hmacShaKeyFor(decodedBytes);
     }
 
 
-    public String createToken(String username) { // role정보도 추가해야된다. UserDto를 아규먼트로 변경해야한다.
+    public String createToken(CustomUserDto userDto) {
         Date currentDateTime = new Date();
+        System.out.println("userDto.getUsername() = " + userDto.getRole());
         return BEARER_PREFIX +
                 Jwts.builder()
-                        .setSubject(username)   // 사용자 식별값
-                        //.claim(AUTHORIZATION_KEY, ) // role
-                        //.claim("EMAIL", ) // email
-                        .claim("username", username)
-                        .setExpiration(new Date(currentDateTime.getTime() + TOKEN_VALIDATION_TIME)) // 유효기간
-                        .setIssuedAt(currentDateTime)   // 발행시간
-                        .signWith(key, signatureAlgorithm)  // 적용 암호화 알고리즘
+                        .setSubject(userDto.getUsername())
+                        .claim("role", userDto.getRole())
+                        .claim("username", userDto.getUsername())
+                        .setExpiration(new Date(currentDateTime.getTime() + TOKEN_VALIDATION_TIME))
+                        .setIssuedAt(currentDateTime)
+                        .signWith(key, signatureAlgorithm)
                         .compact();
     }
 
 
     public HttpServletResponse addJwtToHttpOnlyCookie(HttpServletResponse response, String token, String accessToken) {
-        response.setHeader("Set-Cookie",AUTHORIZATION_HEADER +"=" + token + "; " +
+        response.setHeader("Set-Cookie", HttpHeaders.AUTHORIZATION +"=" + token + "; " +
                                                                 "Path=/;" +
                                                                 "Domain=localhost;" +
                                                                 "HttpOnly; " +
