2b874eb936e60424743b6e7509634caa9db3b96e
kimujae <dnwo0103@naver.com>
2024-07-22 21:58:16 +0900

    KAN-476 feat: 민감정보 프로퍼티 주입

--- src/main/java/com/yeonieum/memberservice/domain/sms/service/SmsVarificationService.java
+++ src/main/java/com/yeonieum/memberservice/domain/sms/service/SmsVarificationService.java
@@ -3,6 +3,7 @@ package com.yeonieum.memberservice.domain.sms.service;
 import com.yeonieum.memberservice.domain.sms.dto.SmsRequest;
 import lombok.RequiredArgsConstructor;
 import net.nurigo.sdk.message.model.Message;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +14,15 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class SmsVarificationService {
 
+    @Value("${sms.fromPhoneNumber}")
+    private String fromPhoneNumber;
+
     private final RedisTemplate<String, String> redisTemplate;
 
     public Message writeVerificationCode(SmsRequest smsRequest) {
         // 인증번호 발송 로직
         Message message = new Message();
-        message.setFrom("01089387607");
+        message.setFrom(fromPhoneNumber);
         message.setTo(smsRequest.getPhoneNumber());
         int generateCode = generateCode();
         message.setText("[연이음] 인증번호 " + generateCode + "를 입력하세요.");
--- src/main/java/com/yeonieum/memberservice/web/controller/SmsVerificationController.java
+++ src/main/java/com/yeonieum/memberservice/web/controller/SmsVerificationController.java
@@ -4,6 +4,7 @@ import com.yeonieum.memberservice.domain.sms.dto.SmsRequest;
 import com.yeonieum.memberservice.domain.sms.service.SmsVarificationService;
 import com.yeonieum.memberservice.global.responses.ApiResponse;
 import com.yeonieum.memberservice.global.responses.code.SuccessCode;
+import jakarta.annotation.PostConstruct;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.exception.NurigoEmptyResponseException;
 import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
@@ -17,21 +18,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/sms")
 public class SmsVerificationController {
-    final DefaultMessageService defaultMessageService;
+    DefaultMessageService defaultMessageService;
     private final SmsVarificationService smsVarificationService;
 
-    //@Value("${sms.apiKey}")
-    private String apiKey = "NCS9GZ2N3ZABQ45D";
-    //@Value("${sms.secretKey}")
-    private String secretKey = "D8CSI6VVIV9HXBXRBRKEO5VJXGRGG12V";
-    //@Value("${sms.apiUrl}")
-    private String apiUrl = "https://api.coolsms.co.kr";
+    @Value("${sms.apiKey}")
+    private String apiKey;
+    @Value("${sms.secretKey}")
+    private String secretKey;
+    @Value("${sms.apiUrl}")
+    private String apiUrl;
 
     public SmsVerificationController(SmsVarificationService smsVarificationService) {
         this.smsVarificationService = smsVarificationService;
-        this.defaultMessageService = NurigoApp.INSTANCE.initialize(apiKey, secretKey, apiUrl);
     }
 
+    @PostConstruct
+    public void init() {
+        defaultMessageService = NurigoApp.INSTANCE.initialize(apiKey, secretKey, apiUrl);
+    }
 
     // 인증번호 발송 API
     @GetMapping("/verification-code")
