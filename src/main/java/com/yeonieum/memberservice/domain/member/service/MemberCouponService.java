a0373a827cbf5722c992e123a3692949884e4f80
Koo-Tae-Ho <gth0908@naver.com>
2024-07-29 13:53:53 +0900

    KAN-606 feat: 회원 예외처리 정의

--- src/main/java/com/yeonieum/memberservice/domain/member/service/MemberCouponService.java
+++ src/main/java/com/yeonieum/memberservice/domain/member/service/MemberCouponService.java
@@ -2,16 +2,21 @@ package com.yeonieum.memberservice.domain.member.service;
 
 import com.yeonieum.memberservice.domain.member.dto.MemberResponse;
 import com.yeonieum.memberservice.domain.member.entity.MemberCoupon;
+import com.yeonieum.memberservice.domain.member.exception.MemberException;
 import com.yeonieum.memberservice.domain.member.repository.MemberCouponRepository;
 import com.yeonieum.memberservice.domain.member.repository.MemberRepository;
 import com.yeonieum.memberservice.global.enums.ActiveStatus;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.yeonieum.memberservice.domain.member.exception.MemberExceptionCode.MEMBER_COUPON_NOT_FOUND;
+import static com.yeonieum.memberservice.domain.member.exception.MemberExceptionCode.MEMBER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -23,14 +28,14 @@ public class MemberCouponService {
     /**
      * 회원의 쿠폰 조회
      * @param memberId 회원 ID
-     * @throws IllegalArgumentException 존재하지 않는 회원 ID인 경우
+     * @throws MemberException 존재하지 않는 회원 ID인 경우
      * @return 회원의 쿠폰 목록
      */
     @Transactional
     public List<MemberResponse.OfRetrieveMemberCoupon> retrieveMemberCoupons (String memberId){
 
         if(!memberRepository.existsById(memberId)){
-            throw new IllegalArgumentException("존재하지 않는 회원 ID 입니다.");
+            throw new MemberException(MEMBER_NOT_FOUND, HttpStatus.NOT_FOUND);
         }
 
         LocalDate currentDate = LocalDate.now();
@@ -46,12 +51,13 @@ public class MemberCouponService {
     /**
      * 회원의 쿠폰 사용 서비스
      * @param memberCouponId
+     * @throws MemberException 존재하지 않는 회원 쿠폰 ID인 경우
      * @return
      */
     @Transactional
     public boolean useMemberCouponStatus(Long memberCouponId) {
         MemberCoupon memberCoupon = memberCouponRepository.findById(memberCouponId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원 ID 입니다."));
+                .orElseThrow(() -> new MemberException(MEMBER_COUPON_NOT_FOUND, HttpStatus.NOT_FOUND));
 
         if (memberCoupon.getIsUsed().equals(ActiveStatus.ACTIVE)) {
             return false;
@@ -63,12 +69,13 @@ public class MemberCouponService {
     /**
      * 주문 실패 시 보상 트랜잭션
      * @param memberCouponId
+     * @throws MemberException 존재하지 않는 회원 쿠폰 ID인 경우
      * @return
      */
     @Transactional
     public boolean unUseMemberCouponStatus(Long memberCouponId) {
         MemberCoupon memberCoupon = memberCouponRepository.findById(memberCouponId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원 ID 입니다."));
+                .orElseThrow(() -> new MemberException(MEMBER_COUPON_NOT_FOUND, HttpStatus.NOT_FOUND));
 
         if (memberCoupon.getIsUsed().equals(ActiveStatus.INACTIVE)) {
             return false;
