a4924f1a9e9ec169e8ffe566bb4cd9a26a544c21
kimujae <dnwo0103@naver.com>
2024-07-30 14:56:28 +0900

    KAN-636 feat: MemberStoreService 판매업체 가입 서비스 구현

--- src/main/java/com/yeonieum/memberservice/domain/memberstore/service/MemberStoreService.java
+++ src/main/java/com/yeonieum/memberservice/domain/memberstore/service/MemberStoreService.java
@@ -1,14 +1,17 @@
 package com.yeonieum.memberservice.domain.memberstore.service;
 
+import com.yeonieum.memberservice.domain.member.entity.Member;
+import com.yeonieum.memberservice.domain.member.entity.MemberStoreId;
+import com.yeonieum.memberservice.domain.member.repository.MemberRepository;
 import com.yeonieum.memberservice.domain.memberstore.dto.MemberStoreResponse;
 import com.yeonieum.memberservice.domain.memberstore.entity.MemberStore;
 import com.yeonieum.memberservice.domain.memberstore.repository.MemberStoreRepository;
 import com.yeonieum.memberservice.global.enums.Gender;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
@@ -17,6 +20,7 @@ import java.time.LocalDate;
 public class MemberStoreService {
 
     private final MemberStoreRepository memberStoreRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 고객의 회원 조회
@@ -28,5 +32,33 @@ public class MemberStoreService {
     public Page<MemberStoreResponse.OfRetrieveMemberInformation> retrieveStoreMembers(Long customerId, String memberId, String memberName, String memberEmail, String memberPhoneNumber, LocalDate memberBirthday, Gender gender, Pageable pageable) {
         Page<MemberStore> memberStores = memberStoreRepository.findByCriteria(customerId, memberId, memberName, memberEmail, memberPhoneNumber, memberBirthday, gender, pageable);
         return memberStores.map(MemberStoreResponse.OfRetrieveMemberInformation::convertedBy);
+    }
+
+
+    /**
+     * 고객 상점에 회원 가입
+     * @param customerId
+     * @param memberId
+     */
+    @Transactional
+    public void joinMemberStore(Long customerId, String memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 회원입니다.")
+        );
+
+        MemberStoreId memberStoreId = MemberStoreId.builder()
+                        .memberId(memberId)
+                        .customerId(customerId)
+                        .build();
+
+        if(memberStoreRepository.findByMemberStoreId(memberStoreId).isPresent()) {
+            throw new IllegalArgumentException("이미 가입된 회원입니다.");
+        }
+
+        memberStoreRepository.save(MemberStore.builder()
+                .memberStoreId(memberStoreId)
+                .member(member)
+                .build()
+        );
     }
 }
