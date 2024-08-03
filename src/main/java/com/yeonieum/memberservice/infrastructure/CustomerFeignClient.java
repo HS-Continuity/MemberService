6283f2f828ebd77d6d194fbb8c75281235ce45ce
kimujae <dnwo0103@naver.com>
2024-08-03 12:34:41 +0900

    KAN-701 dev: 개발 전환 브랜치

--- src/main/java/com/yeonieum/memberservice/infrastructure/CustomerFeignClient.java
+++ src/main/java/com/yeonieum/memberservice/infrastructure/CustomerFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-@FeignClient(name = "productservice", configuration = FeignConfig.class)
+@FeignClient(name = "productservice", url= "localhost:8020", configuration = FeignConfig.class)
 public interface CustomerFeignClient {
     @GetMapping("/productservice/api/customer/auth/{businessNumber}")
     ResponseEntity<ApiResponse<RetrieveCustomerResponse>> retrieveCustomerForAuth(@PathVariable("businessNumber") String businessNumber);
