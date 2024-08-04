55a6cc544b5e447bd6859015358f8dc43f490337
kimujae <dnwo0103@naver.com>
2024-08-05 02:01:59 +0900

    KAN-701 hotfix: 고객 및 사용자 별 액세스토큰 발급 절차 변경

--- src/main/java/com/yeonieum/memberservice/global/config/SecurityConfig.java
+++ src/main/java/com/yeonieum/memberservice/global/config/SecurityConfig.java
@@ -91,6 +91,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
