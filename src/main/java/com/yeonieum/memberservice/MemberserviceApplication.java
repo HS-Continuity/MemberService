a9aeb8f66b24f552d5a5f01281f02bfb316ecd66
Koo-Tae-Ho <gth0908@naver.com>
2024-08-05 21:48:33 +0900

    KAN-731 feat: 회원엔티티 JpaAuditing 어노테이션 추가

--- src/main/java/com/yeonieum/memberservice/MemberserviceApplication.java
+++ src/main/java/com/yeonieum/memberservice/MemberserviceApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableJpaAuditing
 public class MemberserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemberserviceApplication.class, args);
 	}
-
 }
